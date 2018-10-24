70335a880e8abd9fbddc6c7540f01737bffe5f30
lihaoran <toyhaoran@gmail.com>
2018-10-24 17:06:31 +0800

    更新

--- com/numpy/demo01.py
+++ com/numpy/demo01.py
@@ -1,6 +1,20 @@
 
 import numpy as np
 
+"""
+    numpy的优点
+    numpy(Numerical Python)是基于C语言的，可以加快数据分析速度。
+    
+    使用NumPy，开发人员可以执行以下操作:
+    - 数组的数学和逻辑运算。
+    - 用于形状操纵的傅里叶变换和例程。
+    - 与线性代数有关的操作。NumPy具有线性代数和随机数生成的内置函数。
+    
+    NumPy经常与SciPy（Scientific Python）和Mat-plotlib（绘图库）等软件包一起使用。
+    这种组合被广泛用作MatLab的替代品，MatLab是一种流行的技术计算平台。
+    但是，Python替代MatLab现在被视为一种更现代和完整的编程语言。
+"""
+
 ndarray_对象简介 = 0
 # https://www.tutorialspoint.com/numpy/numpy_ndarray_object.htm
 if 0:
